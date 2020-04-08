04c9b7a7db2586394423751d2dec8c4c872dc91d
barelyfitz <pat@barelyfitz.com>
2020-04-08 15:44:27 -0400

    Add storybook

--- src/TDD/__stories__/Pagination.stories.js
+++ src/TDD/__stories__/Pagination.stories.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import { searchLargeStories } from 'ui/shared/storybook'
+import React from 'react';
 import Pagination from '../Pagination'
+// eslint-disable-next-line no-unused-vars
+import styles from './Pagination.stories.css'
+
+export default {
+  title: 'Pagination',
+  component: Pagination,
+}
 
 // Example prop values
 const props = {
@@ -9,9 +15,8 @@ const props = {
   nextUrl: 'next',
 }
 
-searchLargeStories('TDD', module)
-  .add('no label', () => <Pagination />)
-  .add('label only', () => <Pagination label={props.label} />)
-  .add('previousUrl only', () => <Pagination label={props.label} previousUrl={props.previousUrl} />)
-  .add('nextUrl only', () => <Pagination label={props.label} nextUrl={props.nextUrl} />)
-  .add('nextUrl and previousUrl', () => <Pagination {...props} />)
+export const PrevAndNext = () => <Pagination {...props} />
+export const PrevOnly = () => <Pagination label={props.label} previousUrl={props.previousUrl} />
+export const NextOnly = () => <Pagination label={props.label} nextUrl={props.nextUrl} />
+export const LabelOnly = () => <Pagination label={props.label} />
+export const NoLabel = () => <Pagination previousUrl={props.previousUrl} />
