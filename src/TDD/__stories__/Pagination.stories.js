4e53a664242325ac7354fce9b926039f836486c9
barelyfitz <pat@barelyfitz.com>
2020-04-09 10:06:30 -0400

    Add knobs to storybook

--- src/TDD/__stories__/Pagination.stories.js
+++ src/TDD/__stories__/Pagination.stories.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { withKnobs, text } from "@storybook/addon-knobs";
+
 import Pagination from '../Pagination'
 // eslint-disable-next-line no-unused-vars
 import styles from './Pagination.stories.css'
@@ -6,15 +8,26 @@ import styles from './Pagination.stories.css'
 export default {
   title: 'Pagination',
   component: Pagination,
+  decorators: [withKnobs],
 }
 
 // Example prop values
 const props = {
   label: '11 - 20 of 300 Properties',
-  previousUrl: 'prev',
-  nextUrl: 'next',
+  previousUrl: '/search',
+  nextUrl: '/search?page=3',
 }
 
+// Interactive storybook - use knobs to change values
+export const WithKnobs = () => (
+  <Pagination
+    label={text('label', props.label)}
+    previousUrl={text('previousUrl', props.previousUrl)}
+    nextUrl={text('nextUrl', props.nextUrl)}
+  />
+)
+
+// Various combinations of props
 export const PrevAndNext = () => <Pagination {...props} />
 export const PrevOnly = () => <Pagination label={props.label} previousUrl={props.previousUrl} />
 export const NextOnly = () => <Pagination label={props.label} nextUrl={props.nextUrl} />
