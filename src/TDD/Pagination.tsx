c6cee0750c825590491ad8717c4a5ba71a5790c5
dpm3354 <paul.merrill@beaufortfairmont.com>
2020-04-18 11:09:56 -0400

    Removing all functionality and all tests. Want trainees to start at a different place.

--- src/TDD/Pagination.tsx
+++ src/TDD/Pagination.tsx
@@ -1,50 +1,5 @@
-/**
- * @fileoverview
- * The Pagination component is a very simple "dumb" component with no business
- * logic. It displays a label and navigation links based on the props passed
- * into it, but it does no calculation otherwise.
- */
-import React, { ReactElement } from 'react'
+const Pagination = () => {
 
-/**
- * Props for the Pagination component, which displays something like:
- * 11-20 of 30 Properties <Previous Next>
- * @property label - Text label like "11-20 of 30 Properties".
- * - If null or empty string, then do not render anything for the component.
- * @property prevUrl - The URL for the previous page of results.
- * - Display as the "<Previous" link if present
- * - If null or empty string, then do not render the previous link.
- * @property nextUrl - The URL for the next page of results.
- * - Display as the "Next>" link if present.
- * - If null or empty string, then do not render the next link.
- */
-interface Props {
-  label?: string,
-  previousUrl?: string,
-  nextUrl?: string,
-}
-
-/**
- * Component to display the pagination navigation.
- */
-const Pagination = (props: Props): ReactElement | null => {
-  const {
-    label,
-    nextUrl,
-    previousUrl,
-  } = props
-
-  // Render nothing
-  return null
-
-  // Example:
-  // return (
-  //   <div className="Pagination">
-  //     <span>{label}</span>
-  //     <a href={previousUrl}>&lt;Previous</a>
-  //     <a href={nextUrl}>&lt;Previous</a>
-  //   </div>
-  // )
-}
+};
 
 export default Pagination
--- src/TDD/__tests__/Pagination-test.js
+++ src/TDD/__tests__/Pagination-test.js
@@ -2,43 +2,14 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Pagination from '../Pagination'
 
-// Hints:
-// Run the component with specified props:
-// const wrapper = factory({ label: 'testing' })
-//
-// Find elements within the component:
-// const links = wrapper.find('a')
-// const next = wrapper.find('[data-tid=next]')
-//
-// Check for existence:
-// expect(links.length).toBe(0)
-// expect(next.exists()).toBe(true)
-//
-// Get an attribute:
-// const href = next.prop('href')
-// expect(href).toEqual('/url')
 
 describe('<Pagination />', () => {
-  const defaultProps = {
-    label: '11 - 20 of 300',
-    nextUrl: '/next',
-    previousUrl: '/prev',
-  }
 
-  const factory = props => shallow(<Pagination {...props} />)
+  const factory = props => shallow(<Pagination />);
 
-  // Replace this with your tests
-  describe('example of a test', () => {
-    it('renders a link', () => {
-      const props = {
-        ...defaultProps,
-        label: 'Override label',
-      }
-      const wrapper = factory(props)
-      const link = wrapper.find('a')
+  describe('renders', () => {
+    it('something', () => {
 
-      expect(link.length).toEqual(1)
-      expect(link.prop('href')).toEqual('#')
-    })
-  })
-})
+    });
+  });
+});
